90219867e17865b5b3ed6b06f2a8322042235c95
Minaal S Naik <minaalsnaik@gmail.com>
2026-03-05 03:12:31 +0530

    made some changes in server.js

--- src/ws/server.js
+++ src/ws/server.js
@@ -7,7 +7,7 @@ function sendJson(socket, payload) {
 
 function broadcast(wss, payload) {
     for (const client of wss.clients) {
-        if(client.readyState !== WebSocket.OPEN) return;
+        if(client.readyState !== WebSocket.OPEN) continue;
         client.send(JSON.stringify(payload));  
     }
 }
@@ -15,10 +15,26 @@ function broadcast(wss, payload) {
 export function attachWebSocketServer(server) {
     const wss = new WebSocketServer({ server, path: '/ws' , maxPayload: 1024 * 1024 });  //secu measure against memo abuse, if client request is above 1024*1024, it sends a req
     wss.on('connection', (socket) => {
+        socket.isAlive = true;
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        });
         sendJson(socket, { type: 'Welcome to the WebSocket server!' });
-        
         socket.on('error', console.error);       //prevents crash of server when client connection has an error, it logs the error instead
     });
+
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();
+        });
+    }, 30000);
+
+    wss.on('close', () => {
+        clearInterval(interval);
+    });
+
 
     function broadcastMatchCreate(match) {
         broadcast(wss, { type: 'match_created', data: match });
